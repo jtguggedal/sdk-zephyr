zephyrproject-rtos/zephyr

net: dummy L2 for offloaded ifaces
Adds dummy link layer for offloaded ifaces, allowing
ifaces to directly receive l2_enable calls

Signed-off-by: Georges Oates_Larsen <georges.larsen@nordicsemi.no>

drivers/modem/hl7800.c
@@ -29,6 +29,7 @@ LOG_MODULE_REGISTER(LOG_MODULE_NAME, CONFIG_MODEM_LOG_LEVEL);
 #include <zephyr/net/net_offload.h>
 #include <zephyr/net/net_pkt.h>
 #include <zephyr/net/dns_resolve.h>
+#include <zephyr/net/offloaded_if.h>
 #if defined(CONFIG_NET_IPV6)
 #include "ipv6.h"
 #endif
@@ -6334,8 +6335,8 @@ static void offload_iface_init(struct net_if *iface)
 	}
 }
 
-static struct net_if_api api_funcs = {
-	.init = offload_iface_init,
+static struct offloaded_if_api api_funcs = {
+	.iface_api.init = offload_iface_init,
 };
 
 NET_DEVICE_DT_INST_OFFLOAD_DEFINE(0, hl7800_init, NULL, &ictx,

drivers/modem/simcom-sim7080.c
@@ -7,6 +7,7 @@
 #define DT_DRV_COMPAT simcom_sim7080
 
 #include <zephyr/logging/log.h>
+#include <zephyr/net/offloaded_if.h>
 LOG_MODULE_REGISTER(modem_simcom_sim7080, CONFIG_MODEM_LOG_LEVEL);
 
 #include <zephyr/drivers/modem/simcom-sim7080.h>
@@ -760,8 +761,8 @@ const struct socket_dns_offload offload_dns_ops = {
 	.freeaddrinfo = offload_freeaddrinfo,
 };
 
-static struct net_if_api api_funcs = {
-	.init = modem_net_iface_init,
+static struct offloaded_if_api api_funcs = {
+	.iface_api.init = modem_net_iface_init,
 };
 
 static bool offload_is_supported(int family, int type, int proto)

drivers/modem/ublox-sara-r4.c
@@ -19,6 +19,7 @@ LOG_MODULE_REGISTER(modem_ublox_sara_r4, CONFIG_MODEM_LOG_LEVEL);
 
 #include <zephyr/net/net_if.h>
 #include <zephyr/net/net_offload.h>
+#include <zephyr/net/offloaded_if.h>
 #include <zephyr/net/socket_offload.h>
 
 #if defined(CONFIG_MODEM_UBLOX_SARA_AUTODETECT_APN)
@@ -2103,8 +2104,8 @@ static void modem_net_iface_init(struct net_if *iface)
 	net_if_socket_offload_set(iface, offload_socket);
 }
 
-static struct net_if_api api_funcs = {
-	.init = modem_net_iface_init,
+static struct offloaded_if_api api_funcs = {
+	.iface_api.init = modem_net_iface_init,
 };
 
 static const struct modem_cmd response_cmds[] = {

drivers/modem/wncm14a2a.c
@@ -25,6 +25,7 @@ LOG_MODULE_REGISTER(LOG_DOMAIN);
 #include <zephyr/net/net_context.h>
 #include <zephyr/net/net_if.h>
 #include <zephyr/net/net_offload.h>
+#include <zephyr/net/offloaded_if.h>
 #include <zephyr/net/net_pkt.h>
 #if defined(CONFIG_NET_IPV6)
 #include "ipv6.h"
@@ -1773,8 +1774,8 @@ static void offload_iface_init(struct net_if *iface)
 	ctx->iface = iface;
 }
 
-static struct net_if_api api_funcs = {
-	.init	= offload_iface_init,
+static struct offloaded_if_api api_funcs = {
+	.iface_api.init = offload_iface_init,
 };
 
 NET_DEVICE_DT_INST_OFFLOAD_DEFINE(0, wncm14a2a_init, NULL,

include/zephyr/net/net_if.h
@@ -2458,6 +2458,7 @@ struct net_if_api {
 				NET_IF_DEV_GET_NAME(dev_id, sfx)) = {	\
 		.dev = &(DEVICE_NAME_GET(dev_id)),			\
 		.mtu = _mtu,						\
+		.l2 = &(NET_L2_GET_NAME(OFFLOADED_IF)),			\
 	};								\
 	static Z_DECL_ALIGN(struct net_if)				\
 		NET_IF_GET_NAME(dev_id, sfx)[NET_IF_MAX_CONFIGS]	\

include/zephyr/net/wifi_mgmt.h
@@ -15,6 +15,7 @@
 #include <zephyr/net/net_mgmt.h>
 #include <zephyr/net/wifi.h>
 #include <zephyr/net/ethernet.h>
+#include <zephyr/net/offloaded_if.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -159,7 +160,7 @@ struct net_wifi_mgmt_offload {
 #ifdef CONFIG_WIFI_USE_NATIVE_NETWORKING
 	struct ethernet_api wifi_iface;
 #else
-	struct net_if_api wifi_iface;
+	struct offloaded_if_api wifi_iface;
 #endif
 
 	/* cb parameter is the cb that should be called for each

jtguggedal/sdk-zephyr

drivers/modem/hl7800.c
@@ -29,6 +29,7 @@ LOG_MODULE_REGISTER(LOG_MODULE_NAME, CONFIG_MODEM_LOG_LEVEL);
 #include <zephyr/net/net_offload.h>
 #include <zephyr/net/net_pkt.h>
 #include <zephyr/net/dns_resolve.h>
+#include <zephyr/net/offloaded_netdev.h>
 #if defined(CONFIG_NET_IPV6)
 #include "ipv6.h"
 #endif
@@ -6293,8 +6294,8 @@ static void offload_iface_init(struct net_if *iface)
 	}
 }
 
-static struct net_if_api api_funcs = {
-	.init = offload_iface_init,
+static struct offloaded_if_api api_funcs = {
+	.iface_api.init = offload_iface_init,
 };
 
 NET_DEVICE_DT_INST_OFFLOAD_DEFINE(0, hl7800_init, NULL, &ictx,

drivers/modem/simcom-sim7080.c
@@ -7,6 +7,7 @@
 #define DT_DRV_COMPAT simcom_sim7080
 
 #include <zephyr/logging/log.h>
+#include <zephyr/net/offloaded_netdev.h>
 LOG_MODULE_REGISTER(modem_simcom_sim7080, CONFIG_MODEM_LOG_LEVEL);
 
 #include <zephyr/drivers/modem/simcom-sim7080.h>
@@ -760,8 +761,8 @@ const struct socket_dns_offload offload_dns_ops = {
 	.freeaddrinfo = offload_freeaddrinfo,
 };
 
-static struct net_if_api api_funcs = {
-	.init = modem_net_iface_init,
+static struct offloaded_if_api api_funcs = {
+	.iface_api.init = modem_net_iface_init,
 };
 
 static bool offload_is_supported(int family, int type, int proto)

drivers/modem/ublox-sara-r4.c
@@ -19,6 +19,7 @@ LOG_MODULE_REGISTER(modem_ublox_sara_r4, CONFIG_MODEM_LOG_LEVEL);
 
 #include <zephyr/net/net_if.h>
 #include <zephyr/net/net_offload.h>
+#include <zephyr/net/offloaded_netdev.h>
 #include <zephyr/net/socket_offload.h>
 
 #if defined(CONFIG_MODEM_UBLOX_SARA_AUTODETECT_APN)
@@ -2103,8 +2104,8 @@ static void modem_net_iface_init(struct net_if *iface)
 	net_if_socket_offload_set(iface, offload_socket);
 }
 
-static struct net_if_api api_funcs = {
-	.init = modem_net_iface_init,
+static struct offloaded_if_api api_funcs = {
+	.iface_api.init = modem_net_iface_init,
 };
 
 static const struct modem_cmd response_cmds[] = {

drivers/modem/wncm14a2a.c
@@ -25,6 +25,7 @@ LOG_MODULE_REGISTER(LOG_DOMAIN);
 #include <zephyr/net/net_context.h>
 #include <zephyr/net/net_if.h>
 #include <zephyr/net/net_offload.h>
+#include <zephyr/net/offloaded_netdev.h>
 #include <zephyr/net/net_pkt.h>
 #if defined(CONFIG_NET_IPV6)
 #include "ipv6.h"
@@ -1773,8 +1774,8 @@ static void offload_iface_init(struct net_if *iface)
 	ctx->iface = iface;
 }
 
-static struct net_if_api api_funcs = {
-	.init	= offload_iface_init,
+static struct offloaded_if_api api_funcs = {
+	.iface_api.init = offload_iface_init,
 };
 
 NET_DEVICE_DT_INST_OFFLOAD_DEFINE(0, wncm14a2a_init, NULL,

include/zephyr/net/net_if.h
@@ -2458,6 +2458,7 @@ struct net_if_api {
 				NET_IF_DEV_GET_NAME(dev_id, sfx)) = {	\
 		.dev = &(DEVICE_NAME_GET(dev_id)),			\
 		.mtu = _mtu,						\
+		.l2 = &(NET_L2_GET_NAME(OFFLOADED_NETDEV)),		\
 	};								\
 	static Z_DECL_ALIGN(struct net_if)				\
 		NET_IF_GET_NAME(dev_id, sfx)[NET_IF_MAX_CONFIGS]	\

include/zephyr/net/wifi_mgmt.h
@@ -15,6 +15,7 @@
 #include <zephyr/net/net_mgmt.h>
 #include <zephyr/net/wifi.h>
 #include <zephyr/net/ethernet.h>
+#include <zephyr/net/offloaded_netdev.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -246,7 +247,7 @@ struct net_wifi_mgmt_offload {
 #ifdef CONFIG_WIFI_USE_NATIVE_NETWORKING
 	struct ethernet_api wifi_iface;
 #else
-	struct net_if_api wifi_iface;
+	struct offloaded_if_api wifi_iface;
 #endif
 
 	/* cb parameter is the cb that should be called for each